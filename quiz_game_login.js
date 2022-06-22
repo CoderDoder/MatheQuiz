53fa3ed3806150a6d725d80da35ae8599f873d4d
CoderDoder <91969955+CoderDoder@users.noreply.github.com>
2022-06-22 19:29:19 +0530

    Add files via upload

--- quiz_game_login.js
+++ quiz_game_login.js
@@ -4,7 +4,7 @@ function adduser(){
     localStorage.setItem("player1_name",player1_name);
     localStorage.setItem("player2_name",player2_name);
     if(player1_name != "" && player2_name != ""){
-        window.location="quiz_game_page.html";
+        window.location="game-page-index.html";
     }
     else{
         window.alert("please enter username");
